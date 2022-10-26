7a7a20880d128e529318e405309bf381f28bcf3d
Mikhail <mishanyaveselyi@gmail.com>
2022-10-25 22:08:00 -0400

    Import Scrips and Link to first post

--- nextjs-blog1/pages/posts/first-post.js
+++ nextjs-blog1/pages/posts/first-post.js
@@ -1,11 +1,28 @@
+import Script from 'next/script'
+import Head from "next/head"
 import Link from "next/link"
+import Image from "next/image"
 
 export default function FirstPost(){
   return(
     <>
+    <Head>
+      <title> First Post</title>
+      <Script 
+      src="https://connect.facebook.net/en_US/sdk.js"
+      strategy='lazyOnLoad'
+      onLoad={()=>
+      console.log("Script loaded correctly!!!")}
+      />
+    </Head>
       <h1>First Post!</h1>
       <Link href="/">Back to main page </Link>
       <img  style={{heigth: '30%', width:'30%'}} src="/images/Ava.png" />
+      <Image
+        src="/images/Ava.png" 
+        width={30}
+        height={30}
+      />
     </>
   ) 
 }
